7df9a053238036cf8b9e2c4ad97f1728253b5beb
Tomoki Kyotani <tkyotani121@gmail.com>
2019-11-17 15:10:24 -0600

    Create a new Tweet button and add a onclick event to it

--- Twitter/index.js
+++ Twitter/index.js
@@ -1,3 +1,13 @@
 function newTweet(){
-    
+    let text = document.getElementById("newtweet_input");
+    console.log(text.value);
 }
+
+
+
+/* function addEvents(){
+    let tweetbtn = document.getElementById("tweetbtn");
+    tweetbtn.addEventListener("click", newTweet);
+}
+
+window.addEventListener("load", addEvents, false); */
